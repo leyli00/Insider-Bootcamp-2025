3e7a2bb9129f726bc9e651f9b452cf58804ba39b
Leyli Hudayberdiyeva <leylidostluk@gmail.com>
2025-03-02 23:58:50 +0300

    Week2-Day4-Homework

--- Hafta_2/Day4/script.js
+++ Hafta_2/Day4/script.js
@@ -1,61 +1,174 @@
+let task_form = document.querySelector(".blur-effect");
 let add_btn = document.getElementById("add-btn");
-let main_element = document.getElementsByTagName("main")[0];
+
+
 
 add_btn.addEventListener("click", () => {
-    /* Created elements for form */
-    let form_container = document.createElement("div");
-    let label_header_input = document.createElement("label");
-    let task_header_input = document.createElement("input");
-    let label_textarea = document.createElement("label");
-    let desc_textarea = document.createElement("textarea");
-    let blur_effect = document.createElement("div");
-    let importance_label = document.createElement("label");
-    let importance_section = document.createElement("section");
-    let importance_radio1 = document.createElement("input");
-    let importance_radio2 = document.createElement("input");
-    let importance_radio3 = document.createElement("input");
 
-    /* classNames for created elements*/
-    blur_effect.className = "blur-effect";
-    form_container.className = "task-form";
-    label_header_input.className = "label-header-input";
-    task_header_input.className = "task-header-input";
-    label_textarea.className = "label-textarea";
-    desc_textarea.className = "desc-textarea";
-    importance_label.className = "importance-label";
-    importance_section.className = "importance-section";
-    importance_radio1.className = "radio-input";
-
-
-    blur_effect.style.display = "block";
-    label_header_input.innerText = "Enter title (required)";
-    task_header_input.required = "required"
-    label_textarea.innerText = "Please write a description(optional)";
-    desc_textarea.cols = 44;
-    desc_textarea.rows = 6;
-    importance_label.innerText = "Importance";
-    importance_radio1.type = "radio";
-    importance_radio2.type = "radio";
-    importance_radio3.type = "radio";
-    importance_radio1.value = "High";
-    importance_radio2.value = "Middle";
-    importance_radio3.value = "Low";
-    importance_radio1.name = "option"
-    importance_radio2.name = "option"
-    importance_radio3.name = "option"
-
-
-
-
-
-    main_element.appendChild(blur_effect);
-    main_element.appendChild(form_container);
-    form_container.appendChild(label_header_input).appendChild(task_header_input);
-    form_container.appendChild(label_textarea).appendChild(desc_textarea);
-    form_container.appendChild(importance_label).appendChild(importance_section);
-    importance_section.appendChild(importance_radio1);
-    importance_section.appendChild(importance_radio2);
-    importance_section.appendChild(importance_radio3);
-
+    task_form.style.display = "flex";
     console.log("clicked");
 })
+
+// verileri almak için
+
+let task_title = document.querySelector(".task-header-input");
+let task_description = document.querySelector(".desc-textarea");
+let radio_inputs = document.getElementsByClassName("radio-input");
+let create_task_btn = document.querySelector(".create-btn");
+
+let todos = []
+
+// bir todo ekler
+create_task_btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    add_todo()
+    console.log(todos)
+    todo_list_render()
+})
+
+//todo'da bir arama yapar
+let search_input = document.querySelector("#search-input")
+
+search_input.addEventListener('input', (e) => {
+    const search_todos = todos.filter((todo) => {
+        return todo.task_title.includes(search_input.value);
+    });
+    todo_list_render(search_todos)
+});
+
+// bir todo siler
+let todo_list = document.querySelector(".todo-list");
+
+todo_list.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-close')) {
+        e.stopPropagation();
+        const filtered_todos = todos.filter((todo) => {
+            return todo.task_id != e.target.parentElement.getAttribute('task_id');
+        });
+        todos = filtered_todos
+        todo_list_render(todos)
+    }
+});
+
+const add_todo = () => {
+    let isTitleEmpty = true, isSelectedRadio = false;
+
+    let selected_radio;
+
+    if (task_title.value !== "") {
+        isTitleEmpty = false;
+    }
+
+    for (const radio of radio_inputs) {
+        if (radio.checked) {
+            isSelectedRadio = true;
+            selected_radio = radio.value;
+        }
+    }
+
+    // array'a bir todo ekler
+    if (isTitleEmpty === false && isSelectedRadio === true) {
+        todos.push({
+            task_title: task_title.value,
+            task_description: task_description.value.trim(),
+            task_importance: selected_radio,
+            isCompleted: false
+        })
+
+        // form elemanlarının değerlerini sıfırlar
+        reset_popup();
+
+    }
+
+    else {
+        console.log("Title veya önem derecesi seçilmemiş / düzenlenecek burası")
+    }
+}
+
+const reset_popup = () => {
+    isTitleEmpty = true, isSelectedRadio = false
+    task_title.value = "";
+    task_description.value = "";
+    for (const radio of radio_inputs) {
+        if (radio.checked) {
+            radio.checked = false;
+        }
+    }
+    task_form.style.display = "none";
+    search_input.value = "";
+}
+
+
+// todoları listeler
+const todo_list_render = () => {
+
+    let todo_list = document.querySelector(".todo-list");
+    todo_list.innerHTML = " ";
+
+    for (const todo of todos) {
+        let todo_list_item = document.createElement("div");
+        todo_list.appendChild(todo_list_item);
+        todo_list_item.className = "todo-list-item";
+
+        //checkbox ekler
+        let isCompletedCheckBox = document.createElement("input");
+        isCompletedCheckBox.classList.add("isCompletedCheckBox");
+        isCompletedCheckBox.type = "checkbox";
+        isCompletedCheckBox.name = "option"
+        isCompletedCheckBox.value = todo_list_item;
+        todo_list_item.appendChild(isCompletedCheckBox);
+
+        //Todo başlığı ekler
+        let todo_title_div = document.createElement("div");
+        todo_title_div.classList.add("todo_title_div");
+        todo_title_div.innerHTML = todo.task_title;
+        todo_list_item.appendChild(todo_title_div);
+
+        // 
+        let task_title_checkbox = document.createElement("span");
+        task_title_checkbox.className = "task-title-checkbox-span";
+        todo_list_item.appendChild(task_title_checkbox);
+        task_title_checkbox.appendChild(isCompletedCheckBox);
+        task_title_checkbox.appendChild(todo_title_div);
+
+        //Todo açıklaması ekler
+        let todo_desc_p = document.createElement("p");
+        todo_desc_p.classList.add("todo_desc_p");
+        todo_desc_p.innerHTML = todo.task_description;
+        todo_list_item.appendChild(todo_desc_p);
+        todo_desc_p.className = "todo-desc-p";
+
+        //Todo önem derecesi ekler
+        let todo_importance = document.createElement("div");
+        todo_importance.classList.add("todo_importance");
+        todo_importance.innerHTML = todo.task_importance;
+
+        //Todo edit icon ekler
+        let edit_icon = document.createElement("i");
+        edit_icon.classList.add("fa", "fa-edit");
+
+        //Todo remove icon ekler
+        let remove_icon = document.createElement("i");
+        remove_icon.classList.add("fa", "fa-close");
+
+
+        // 3 öğeyi sağa koymak için spana attım
+        let importance_icons = document.createElement("span");
+        importance_icons.className = "importance-icons-span";
+        todo_list_item.appendChild(importance_icons);
+        importance_icons.appendChild(todo_importance);
+        importance_icons.appendChild(edit_icon);
+        importance_icons.appendChild(remove_icon);
+    }
+
+    let complete_btn = document.getElementsByClassName(".complete-btn")[0];
+
+    complete_btn.addEventListener("click", () => {
+        let checkboxes = document.querySelectorAll('.todo_list_item input[type="checkbox"]');
+        let selectedValues = Array.from(checkboxes).filter(checkbox => checked).map(checkbox => checkbox.value);
+
+        console.log(selectedValues);
+    })
+
+}
+
